f4310940c1b6482fd55d714d21febad5c4f4738f
NickTsioutsias <117465959+NickTsioutsias@users.noreply.github.com>
2023-11-23 21:46:30 +0200

    created insert users prototype for visual examples

---
+++ INSERT.sql
@@ -0,0 +1,32 @@
+-- INSERT INTO admin (admin_id, username, password, name, lastname) 
+-- VALUES(
+--   1,
+--   "Admin", 
+--   "Admin!", 
+--   "Giannhs", 
+--   "Papadopoulos", 
+-- );
+
+-- INSERT INTO rescuer (resc_id, username, password, name, lastname, r_cords, car_name) 
+-- VALUES(
+--   2,
+--   "rescuer1",
+--   "rescuer1!",
+--   "Mixalhs",
+--   "Giannakopoulos",
+--   POINT(38.246229, 21.735412),
+--   "Oxhma_1"
+-- );
+
+-- INSERT INTO citizen (citizen_id, username, password, name, lastname, phone, email, address, zip) 
+-- VALUES(
+--   3,
+--   "citizen1",
+--   "citizen1!",
+--   "Petros",
+--   "Dhmiakos",
+--   "6911111111",
+--   "petrosdhmiakos@gmail.com",
+--   "Pantanashs",
+--   10
+-- );
