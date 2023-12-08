794d53798cfe00f5a62e604f60ebcbdbbe23ffd3
NickTsioutsias <117465959+NickTsioutsias@users.noreply.github.com>
2023-12-08 11:31:03 +0200

    moved error handlers to right files

--- main.php
+++ main.php
@@ -70,65 +70,14 @@
 <?php
   // Error Handling
   $fullUrl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-  if(strpos($fullUrl, "signup=empty") == true){
-    echo "You did not fill in all fields!";
-  }
-  elseif(strpos($fullUrl, "signup=wrongusername") == true){
-    echo "Username needs to have alphanumeric characters and _ symbols only, up to 20 characters";
-  }
-  elseif(strpos($fullUrl, "signup=invalidpassword") == true){
-    echo "Password needs to have alphanumeric characters and ! or _ symbols only";
-  }
-  elseif(strpos($fullUrl, "signup=invalidname") == true){
-    echo "Names can only be written in lower and uppercase letters";
-  }
-  elseif(strpos($fullUrl, "signup=invalidlastname") == true){
-    echo "Lastnames can only be written in lower and uppercase letters";
-  }
-  elseif(strpos($fullUrl, "signup=invalidphone") == true){
-    echo "This is not a phone number";
-  }
-  elseif(strpos($fullUrl, "signup=invalidemail") == true){
-    echo "Invalid email";
-  }
-  elseif(strpos($fullUrl, "signup=invalidaddress") == true){
-    echo "Addresses can only be written in uppercase letters";
-  }
-  elseif(strpos($fullUrl, "signup=invalidzip") == true){
-    echo "Invalid zip postal code";
-  }
-  elseif(strpos($fullUrl, "signup=invalidlatitude") == true){
-    echo "Latitude is written like this: 38.246229";
-  }
-  elseif(strpos($fullUrl, "signup=invalidlongtitude") == true){
-    echo "Longtitude is written like this: 21.735412";
-  }
-  elseif(strpos($fullUrl, "error=sqlerror") == true){
+  
+  if(strpos($fullUrl, "error=sqlerror") == true){
     echo "There was an SQL error";
-  }
-  elseif(strpos($fullUrl, "error=usernametaken") == true){
-    echo "This username is taken, or phone number, email already exists";
   }
   elseif(strpos($fullUrl, "signup=success") == true){
     echo "Hooray! You Signed up!";
   }
 
-  elseif(strpos($fullUrl, "error=wrongpwd") == true){
-    echo "Wrong Password";
-  }
-  elseif(strpos($fullUrl, "login=success") == true){
-    echo "Hooray! You Logged in!";
-  }
-  elseif(strpos($fullUrl, "error=nouser") == true){
-    echo "There is no user with this username";
-  }
-  elseif(strpos($fullUrl, "error=noadminuser") == true){
-    echo "There is no admin like that";
-  }
-  elseif(strpos($fullUrl, "signup=invalidcarname") == true){
-    echo "Invalid car name";
-  }
 ?>
 </body>
 </html>
